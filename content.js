6f4da629c6ba21fae4e7044119bb29b13d024ed2
Anna Khan-Akselrod <annakhanakselrod@students.berkeley.net>
2022-02-28 19:16:05 -0800

    first commit

---
+++ content.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("#1").click(function(){
+      $(this).html("0");
+    });
+    $("#2").click(function(){
+      $(this).html("0");
+    });
+    $("#3").click(function(){
+      $(this).html("0");
+    });
+    $("#4").click(function(){
+      $(this).html("0");
+    });
+    $("#5").click(function(){
+      $(this).html("0");
+    });
+    $("#6").click(function(){
+      $(this).html("0");
+    });
+    $("#7").click(function(){
+      $(this).html("0");
+    });
+    $("#8").click(function(){
+      $(this).html("0");
+    });
+    
+  });
---
+++ content1.js
@@ -0,0 +1,42 @@
+var n=parseInt(document.getElementById("txt1"));
+var k=parseInt(document.getElementById("txt2"));
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    document.getElementById("b1").addEventListener('click', oddsOfWinning(n,k)); 
+      });
+   
+
+
+    
+    
+  function oddsOfWinning(n,k){
+    var nFactor = factorialize(n);
+    var kFactor = factorialize(k); 
+    var nMinusK = factorialize(n-k); 
+    if (n >=k){
+      var odds = nFactor/(kFactor * nMinusK); 
+      odds = Math.floor(odds);
+      odds = odds.toLocaleString(); 
+    }
+    alert("Your odds of winning are 1 in " + odds); 
+  }
+
+
+
+function factorialize(num){
+    var result = num;
+    if(num == 0|| num ==1){
+      return 1; 
+    }
+    while (num > 1){
+      num--; 
+      result *= num; 
+    }
+    return (result); 
+  }
+
+
+
+  
+  
